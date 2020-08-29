545d909b892958cf08bf3f425cfa9ce239462a0e
Lee Burton <leeaburton@users.noreply.github.com>
2020-08-29 13:44:23 +0100

    include LB_SHU video in news section

--- news/full-list/myscript.js
+++ news/full-list/myscript.js
@@ -109,3 +109,27 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
   modal.style.display = "none";
 } 
+
+// Get the modal
+var modal = document.getElementById("myModal1");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn6");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+btn.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = "/img/LB_SHU.mp4";
+  captionText.innerHTML = "Lee Burton feature material 2020";
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+} 
