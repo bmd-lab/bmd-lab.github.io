76513f79172ac7cea78d6491ca81f4e610acea91
Lee Burton <leeaburton@users.noreply.github.com>
2021-08-13 14:09:41 +0100

    update group news

--- zh/news/full-list/myscript.js
+++ zh/news/full-list/myscript.js
@@ -133,3 +133,27 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
   modal.style.display = "none";
 } 
+
+// Get the modal
+var modal = document.getElementById("myModal1");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn7");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+btn.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = "/img/conf_2021.jpg";
+  captionText.innerHTML = "International Symposium on Emerging Electronic Materials 2021";
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+} 
