6eee62f5defe36f09e872d85e78b61cc2da70ffe
Lee Burton <leeaburton@users.noreply.github.com>
2021-09-12 10:19:42 +0100

    update people widget for new student and alumni

---
+++ alumni/full-list/myscript.js
@@ -0,0 +1,159 @@
+// Get the modal
+var modal = document.getElementById("myModal1");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn1");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+btn.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = "/img/Summerschool2019.jpg";
+  captionText.innerHTML = "Summer school for Computational Materials Design 2019";
+}
+
+// Get the modal
+var modal = document.getElementById("myModal1");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn2");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+btn.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = "/img/AMDS2019_3.jpg";
+  captionText.innerHTML = "Asian Materials Data Symposium 2019";
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+} 
+
+// Get the modal
+var modal = document.getElementById("myModal1");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn3");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+btn.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = "/img/IWMG2019.jpg";
+  captionText.innerHTML = "International Workshop on Materials Genomics 2019";
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+} 
+
+// Get the modal
+var modal = document.getElementById("myModal1");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn4");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+btn.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = "/img/Xian2019.jpg";
+  captionText.innerHTML = "1st International Symposium on Computational Structure Prediction and Advanced Materials 2019";
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+} 
+
+// Get the modal
+var modal = document.getElementById("myModal1");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn5");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+btn.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = "/img/ISMS.jpg";
+  captionText.innerHTML = "International Symposium on Materials Science 2019";
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+} 
+
+// Get the modal
+var modal = document.getElementById("myModal1");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn6");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+btn.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = "/img/QIF2019.jpg";
+  captionText.innerHTML = "Quantum International Frontiers 2019";
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+} 
+
+// Get the modal
+var modal = document.getElementById("myModal1");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn7");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+btn.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = "/img/conf_2021.jpg";
+  captionText.innerHTML = "International Symposium on Emerging Electronic Materials 2021";
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+} 
