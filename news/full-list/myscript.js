d1e440523b2b33d2856cf7b3930258b4e2f52a7a
Lee Burton <leeaburton@users.noreply.github.com>
2022-08-23 18:13:58 +0100

    update news and publications

--- news/full-list/myscript.js
+++ news/full-list/myscript.js
@@ -157,3 +157,27 @@ var span = document.getElementsByClassName("close")[0];
 span.onclick = function() {
   modal.style.display = "none";
 } 
+
+// Get the modal
+var modal = document.getElementById("myModal1");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn8");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+btn.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = "/img/imperial2022.jpg";
+  captionText.innerHTML = "Departmental Seminar of Imperial College London 2022";
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+} 
